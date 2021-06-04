f7bd491bbbb0801eb7f9cb2a353139046f386428
nikololiahim <olomishcak@outlook.com>
2021-06-04 20:34:30 +0300

    added code coverage

--- src/main/java/org/eolang/App.java
+++ src/main/java/org/eolang/App.java
@@ -2,12 +2,14 @@ package org.eolang;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public void someUncoveredMethod() {
+        System.out.println("I am not covered!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
