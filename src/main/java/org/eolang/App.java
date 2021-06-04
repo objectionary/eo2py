2ede219505c9f076fa0d1bf0340132834d38f212
nikololiahim <olomishcak@outlook.com>
2021-06-04 19:49:25 +0300

    initial commit

---
+++ src/main/java/org/eolang/App.java
@@ -0,0 +1,13 @@
+package org.eolang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
