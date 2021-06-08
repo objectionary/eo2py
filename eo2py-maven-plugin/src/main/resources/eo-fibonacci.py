4a29b6a93c7ccf3a5b2f6f246fff18d246b86f3a
nikololiahim <olomishcak@outlook.com>
2021-06-08 17:46:22 +0300

    moved example program to a separate file

---
+++ eo2py-maven-plugin/src/main/resources/eo-fibonacci.py
@@ -0,0 +1,33 @@
+from atoms import EOInt, EOIf, EOLess, EOSub, EOAdd
+
+
+class Fibo(EOInt):
+    def __init__(self, n):
+        super().__init__("")
+        self.n = n
+
+    def dataize(self):
+        n = self.n
+
+        self.__PHI__ = EOIf(
+            EOLess(n, EOInt("2")),
+            n,
+            EOAdd(
+                Fibo(
+                    EOSub(
+                        n,
+                        EOInt("1")
+                    )),
+                Fibo(
+                    EOSub(
+                        n,
+                        EOInt("2")
+                    ))
+            )
+        )
+
+        return self.__PHI__.dataize()
+
+
+if __name__ == "__main__":
+    print(Fibo(EOInt("3")).dataize())
