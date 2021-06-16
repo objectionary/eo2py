7d502f8ada1b51fbffa84d190c5ed236b1510657
nikololiahim <olomishcak@outlook.com>
2021-06-17 01:09:58 +0300

    copied recursive-dataization/atoms.py to atoms.py

--- eo2py-maven-plugin/src/main/resources/atoms.py
+++ eo2py-maven-plugin/src/main/resources/atoms.py
@@ -8,8 +8,8 @@ class EObase:
     def dataize(self) -> object:
         raise NotImplementedError()
 
-class EOerror(EObase):
 
+class EOerror(EObase):
     def dataize(self) -> object:
         raise NotImplementedError()
 
@@ -17,13 +17,25 @@ class EOerror(EObase):
 class EOnumber(EObase):
     def __init__(self, value: int):
         self.value = value
-        self.add = partial(EOint_EOadd, self)
+        self.add = partial(EOnumber_EOadd, self)
+        self.sub = partial(EOnumber_EOsub, self)
+        self.pow = partial(EOnumber_EOpow, self)
+        self.less = partial(EOnumber_EOless, self)
 
     def dataize(self) -> int:
         return self.value
 
 
-class EOint_EOadd(EOnumber):
+class EObool(EObase):
+    def __init__(self, value: bool):
+        self.value = value
+        self.If = partial(EObool_If, self)
+
+    def dataize(self) -> bool:
+        return self.value
+
+
+class EOnumber_EOadd(EOnumber):
     def __init__(self, parent: EOnumber, other: EOnumber):
         super().__init__(0)
         self.parent = parent
@@ -33,15 +45,59 @@ class EOint_EOadd(EOnumber):
         return self.parent.dataize() + self.other.dataize()
 
 
-class EOint_EOpow(EOnumber):
+class EOnumber_EOsub(EOnumber):
     def __init__(self, parent: EOnumber, other: EOnumber):
         super().__init__(0)
         self.parent = parent
         self.other = other
 
     def dataize(self):
-        return self.parent.dataize() ^ self.other.dataize()
+        return self.parent.dataize() - self.other.dataize()
+
+
+
+class EObool_If(EObase):
+    def __init__(self, parent: EObool, iftrue: EObase, iffalse: EObase):
+        self.parent = parent
+        self.iftrue = iftrue
+        self.iffalse = iffalse
+
+    def dataize(self):
+        return self.iftrue.dataize() if self.parent.dataize() else self.iffalse.dataize()
+
+
+class EOnumber_EOless(EObool):
+    def __init__(self, parent: EOnumber, other: EOnumber):
+        super().__init__(False)
+        self.parent = parent
+        self.other = other
+
+    def dataize(self):
+        return self.parent.dataize() < self.other.dataize()
+
+
+class EOnumber_EOpow(EOnumber):
+    def __init__(self, parent: EOnumber, other: EOnumber):
+        super().__init__(0)
+        self.parent = parent
+        self.other = other
+
+    def dataize(self):
+        return self.parent.dataize() ** self.other.dataize()
+
+
+def lazy_property(fn):
+    attr = "_lazy__" + fn.__name__
+
+    @property
+    def _lazy_property(self):
+        if not hasattr(self, attr):
+            value = fn(self)
+            setattr(self, attr, value)
+        return getattr(self, attr)
+
+    return _lazy_property
 
 
 if __name__ == "__main__":
-    print(EOnumber(2).add(EOnumber(3)).dataize())
+    print(EOnumber(2).pow(EOnumber(10)).dataize())
