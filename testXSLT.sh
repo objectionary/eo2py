2a9e94aedc1ad1a1759d3986c585bdb4b9565297
nikololiahim <olomishcak@outlook.com>
2021-06-22 09:49:37 +0300

    #16 removed script

--- testXSLT.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-cd /home/nikololiahim/IdeaProjects/eo-runtime-python || exit
-mvn clean install
-cd sandbox || exit
-mvn clean compile
-gedit target/05-compile/app.eo.xml &
-gedit target/04-pre/app.eo.xml/??-pre-data.xml &
