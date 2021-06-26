0bbbc7409112d3d414de78f72ab29a5ea448158e
nikololiahim <olomishcak@outlook.com>
2021-06-26 19:19:25 +0300

    updated release script

--- eo-python-runtime/scripts/release.sh
+++ eo-python-runtime/scripts/release.sh
@@ -3,12 +3,6 @@
 cd ..
 rm -rf dist
 python3 -m build
-# TODO: bump version
-#echo "Enter the part of the version you want to update: "
-#read version_part
-#if [[ "$version_part" == "major" || "$version_part" == "minor" || "$version_part" == "patch" ]]; then
-#  bump2version
-#fi
 twine upload --verbose --repository eo2py dist/*
 echo "> Waiting until upload finishes..."
 sleep 12s
