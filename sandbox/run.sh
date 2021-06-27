2a94c9ebc80e1fd69cfd8ac38ca4520491fc657a
nikololiahim <olomishcak@outlook.com>
2021-06-27 23:55:11 +0300

    added script to compile and run .eo files

---
+++ sandbox/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "Activating venv..." && sleep 1s
+source venv/bin/activate
+
+echo "Compiling .eo files..." && sleep 1s
+mvn clean compile
+
+echo "Generated Python Files:" && sleep 1s
+ls  target/generated-sources/
+
+echo "Formatting python files with black..." && sleep 1s
+printf "\n\nif __name__ == \"__main__\":\n    print(EOapp().dataize().data())\n\n" >> target/generated-sources/app.eo.py
+python -m black "$(find target/generated-sources | sed -n '2, $p' | tr '\n' ' ')"
+
+echo "Running Python files..." && sleep 1s
+python target/generated-sources/app.eo.py
+
+echo "Deactivating venv..." && sleep 1s
+deactivate
